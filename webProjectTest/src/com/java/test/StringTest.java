e00d9615d4635c2365761eabc253bcdb41a4fd13
butu <weiyunhong082@126.com>
2017-03-20 10:30:30 +0800

    test
    test

---
+++ webProjectTest/src/com/java/test/StringTest.java
@@ -0,0 +1,16 @@
+package com.java.test;
+
+public class StringTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = "AB汉字C";
+		char[] c = str.toCharArray();
+		System.out.println(str.toCharArray());
+		System.out.println(c[1]);
+		System.out.println(c[3]);
+
+
+	}
+
+}
